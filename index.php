34e7fb1f10f0fc2390f17283e644a153f3f0bc83
skojphy <skojphy@gmail.com>
2021-07-29 23:45:40 +0900

    docs: Minor text corrections

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>점심 장소 정해 드립니다</title>
+    <title>점심 정해 드립니다</title>
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-143196260-1"></script>
